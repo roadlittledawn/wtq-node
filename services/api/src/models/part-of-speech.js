b8dd065ffce87541dab076d256a1f3dbb1671070
Clinton <clinton.langosch@gmail.com>
2018-11-17 15:42:23 -0800

    Fix require

--- services/api/src/models/part-of-speech.js
+++ services/api/src/models/part-of-speech.js
@@ -1,4 +1,4 @@
 const mongoose = require('../connections/db');
-const schema = require('../schema/function');
+const schema = require('../schema/part-of-speech');
 
 module.exports = mongoose.model('function', schema);
