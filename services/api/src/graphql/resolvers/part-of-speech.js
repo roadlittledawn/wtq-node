a3899ff07de94ea4f8233ec3ca698f75a21d15c5
Clinton <clinton.langosch@gmail.com>
2018-11-17 17:27:43 -0800

    Add partOfSpeech query

--- services/api/src/graphql/resolvers/part-of-speech.js
+++ services/api/src/graphql/resolvers/part-of-speech.js
@@ -1,6 +1,12 @@
 const PartOfSpeech = require('../../models/part-of-speech');
 
 module.exports = {
+  Query: {
+    partOfSpeech: (_, { input }) => {
+      const { id } = input;
+      return PartOfSpeech.findById(id);
+    },
+  },
   Mutation: {
     createPartOfSpeech: (_, { input }) => {
       const { name } = input;
