0dcc5e7e87390c570cd5abde7a9d553bf7876960
Clinton <clinton.langosch@gmail.com>
2018-11-17 17:40:11 -0800

    Add update+delete muts; all query

--- services/api/src/graphql/resolvers/part-of-speech.js
+++ services/api/src/graphql/resolvers/part-of-speech.js
@@ -2,6 +2,7 @@ const PartOfSpeech = require('../../models/part-of-speech');
 
 module.exports = {
   Query: {
+    allPartsOfSpeech: () => PartOfSpeech.find(),
     partOfSpeech: (_, { input }) => {
       const { id } = input;
       return PartOfSpeech.findById(id);
@@ -11,6 +12,17 @@ module.exports = {
     createPartOfSpeech: (_, { input }) => {
       const { name } = input;
       return PartOfSpeech.create({ name });
+    },
+    updatePartOfSpeech: async (_, { input }) => {
+      const { id, payload } = input;
+      const partOfSpeech = await PartOfSpeech.findById(id);
+      partOfSpeech.set(payload);
+      return partOfSpeech.save();
+    },
+    deletePartOfSpeech: async (_, { input }) => {
+      const { id } = input;
+      await PartOfSpeech.remove({ _id: id });
+      return true;
     },
   },
 };
