311fcd9e1d9915371770744776acfd2392b1b749
Jacob Bare <jacob@limit0.io>
2018-12-07 12:02:45 -0600

    Fix deprecation notice re: url parser

--- services/api/src/connections/db.js
+++ services/api/src/connections/db.js
@@ -4,6 +4,7 @@ const { MONGO_DSN } = process.env;
 
 const connection = mongoose.createConnection(MONGO_DSN, {
   ignoreUndefined: true,
+  useNewUrlParser: true,
 });
 
 module.exports = connection;
