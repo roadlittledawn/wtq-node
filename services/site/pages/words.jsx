5cf1236dc7c56bd29b4794383c1a45bcfa514fc2
Clinton <clinton.langosch@gmail.com>
2018-12-22 13:27:52 -0800

    Add allWords query to words page

--- services/site/pages/words.jsx
+++ services/site/pages/words.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import Head from 'next/head';
-
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Alert } from 'reactstrap';
 import DefaultLayout from '../layouts/Default';
+
+const allWords = gql`
+  query AllWords {
+    allWords {
+      id
+      name
+      definition
+      note
+      partsOfSpeech {
+        id
+        name
+      }
+    }
+  }
+`;
 
 export default () => (
   <DefaultLayout>
@@ -9,5 +26,16 @@ export default () => (
       <title>Words</title>
     </Head>
     <h1>Words</h1>
+    <Query query={allWords}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <Alert color="danger">{error.message}</Alert>;
+        return data.allWords.map(word => (
+          <div key={word.id}>
+            <p>{word.name}</p>
+          </div>
+        ));
+      }}
+    </Query>
   </DefaultLayout>
 );
