665c2793734930f8adf0c3c7118c8639879e229a
roadlittledawn <clinton.langosch@gmail.com>
2019-01-24 10:56:25 -0800

    require phrase resolver

--- services/api/src/graphql/resolvers/index.js
+++ services/api/src/graphql/resolvers/index.js
@@ -2,6 +2,7 @@ const deepAssign = require('deep-assign');
 const etymology = require('./etymology');
 const partOfSpeech = require('./part-of-speech');
 const word = require('./word');
+const phrase = require('./phrase');
 
 module.exports = deepAssign(
   etymology,
