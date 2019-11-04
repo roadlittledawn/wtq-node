5ecbf2bcdf537d8e50590462de3450edce31950b
Clinton <clinton.langosch@gmail.com>
2019-11-04 11:00:54 -0800

    Add quote query to quotes page
    Remember to update the gql returned fields in order to use them.

--- services/site/pages/quotes.jsx
+++ services/site/pages/quotes.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import Head from 'next/head';
-
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { 
+  Alert, Card, CardText, CardBody,
+  CardTitle, CardSubtitle, Button 
+} from 'reactstrap';
+import Link from 'next/link';
 import DefaultLayout from '../layouts/Default';
+
+const allQuotes = gql`
+  query QuotesPage {
+    allQuotes {
+      id
+      name
+      slug
+      author {
+        name
+      }
+      body
+      note
+    }
+  }
+`;
 
 export default () => (
   <DefaultLayout>
@@ -9,5 +30,28 @@ export default () => (
       <title>Quotes</title>
     </Head>
     <h1>Quotes</h1>
+    <Query query={allQuotes}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <Alert color="danger">{error.message}</Alert>;
+        return data.allQuotes.map(quote => (
+          <div>
+            <Card>
+              <CardBody>
+                <CardTitle>
+                  <Link href={`/quote?slug=${quote.slug}`} as={`/quote/${quote.slug}`}>
+                    <a>{quote.name}</a>
+                  </Link>
+                </CardTitle>
+                <CardText>
+                  {quote.body}
+                </CardText>
+              </CardBody>
+            </Card>
+          </div>
+        ));
+      }}
+    </Query>
+
   </DefaultLayout>
 );
