6458e9baa891442dd236e16369a6122c30eca054
Clinton <clinton.langosch@gmail.com>
2019-11-22 17:13:39 -0800

    Update conditionals to not duplicate headings

--- services/site/pages/word.jsx
+++ services/site/pages/word.jsx
@@ -43,9 +43,12 @@ const WordPage = ({ slug }) => (
             <h1>{word.name}</h1>
             <h2>Definition</h2>
             <p>{word.definition}</p>
-            {word.partsOfSpeech && word.partsOfSpeech.map(item => <><h2>Parts of Speech</h2><p>{item.name}</p></>)}
-            {word.contexts && word.contexts.map(item => <><h2>Contexts</h2><p>{item.name}</p></>)}
-            {word.tones && word.tones.map(item => <><h2>Tones</h2><p>{item.name}</p></>)}
+            {word.partsOfSpeech && <h2>Part of Speech</h2>}
+            {word.partsOfSpeech && word.partsOfSpeech.map(item => <><p>{item.name}</p></>)}
+            {word.partsOfSpeech && <h2>Context</h2>}
+            {word.contexts && word.contexts.map(item => <><p>{item.name}</p></>)}
+            {word.partsOfSpeech && <h2>Tone</h2>}
+            {word.tones && word.tones.map(item => <><p>{item.name}</p></>)}
           </>
         );
       }}
