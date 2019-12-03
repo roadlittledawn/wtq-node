b1e6545886f479c953b919d0fe628f5c0d987384
Clinton <clinton.langosch@gmail.com>
2019-12-03 13:54:33 -0800

    convert to lowercase for comparison
    upper and lower case letters have different values

--- services/site/pages/taxCreate.jsx
+++ services/site/pages/taxCreate.jsx
@@ -260,10 +260,10 @@ class TaxCreateForm extends React.Component {
                       <tbody>
                         {
                         returnObjArr.sort((a, b) => {
-                          if (a.name > b.name) {
+                          if (a.name.toLowerCase() > b.name.toLowerCase()) {
                             return 1;
                           }
-                          if (a.name < b.name) {
+                          if (a.name.toLowerCase() < b.name.toLowerCase()) {
                             return -1;
                           }
                           return 0;
